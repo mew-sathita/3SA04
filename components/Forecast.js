f141263e474f79f42d3eed15325fbe4f1dbad684
mew-sathita <s6135512032@phuket.psu.ac.th>
2020-10-02 20:59:12 +0700

    Weather App

--- components/Forecast.js
+++ components/Forecast.js
@@ -4,31 +4,32 @@ import { View, Text, StyleSheet } from 'react-native';
 export default function Forecast(props) {
     return (
         <View style={styles.center}>
-            <Text style={styles.big}>{props.main}</Text>
-            <Text style={styles.medium}>{props.description}</Text>
-            <View style={{marginTop: 20}}>
-                <Text>
-                    <Text style={styles.big}>{props.temp}  </Text>
-                    <Text style={styles.medium}>°C</Text>
-                </Text>
-            </View>
+        <Text style={styles.medium}>{props.main}</Text>
+        <Text style={styles.small}>{props.description}</Text>
+        <View>
+            <Text style={styles.medium}>
+                <Text style={{fontSize: 100}}>{props.temp}</Text>
+                <Text>  °C</Text>
+            </Text>
         </View>
+    </View>              
     );
 }
 
 const styles = StyleSheet.create({
     center: {
-        justifyContent: 'space-around',
-        alignItems: 'center',
+        alignItems: 'center'
     },
-    big: {
-        marginTop: 20,
-        fontSize: 30,
-        color: 'white',
-    },
+    //wt-top+temp
     medium: {
-        marginTop: 20,
-        fontSize: 15,
-        color: 'white',
+        fontSize: 25,
+        color: '#264d73',
+        marginTop: 5,
+    },
+    //wt-under
+    small: {
+        fontSize: 30,
+        color: '#264d73',
+        marginTop: 5,
     }
-})
+});
--- components/ZipCodeScreen.js
+++ components/ZipCodeScreen.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { View, Text, FlatList, TouchableHighlight } from 'react-native';
+import { View, Text, FlatList, TouchableHighlight, StyleSheet,ImageBackground} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 const availableZipItems = [
+    { place: 'Kathu, Phuket', code: '83120' },
+    { place: 'Bangkok', code: '10100' },
+    { place: 'Chiangmai', code: '50000' },
     { place: 'Hatyai', code: '90110' },
     { place: 'Trang', code: '92000' },
-    { place: 'Chiangmai', code: '50000' },
     { place: 'Khonkaen', code: '40000' },
     { place: 'Chonburi', code: '20000' },
+    { place: 'Chumphon', code: '86000' },
+    { place: 'Phayao', code: '56000' },
+    { place: 'Phatthalung', code: '93000' },
+    { place: 'Satun', code: '91000' },
+    { place: 'BuriRam', code: '31000' },
+    { place: 'Ranong', code: '85000' }
 ]
-
+                                
 const ZipItem = ({ place, code, navigation }) => (
-    <TouchableHighlight onPress={() => navigation.navigate('Weather', { zipCode: code })}>
-        <View style={{ borderColor: 'red', borderWidth: 3 }}>
-            <Text>{place}</Text>
-            <Text>{code}</Text>
-        </View>
-    </TouchableHighlight>
+    //color touch
+  <TouchableHighlight onPress={() => navigation.navigate('Weather', { zipCode: code })} underlayColor="#ffcce6"> 
+  <View style={styles.container}>
+      <Text>{place}</Text>
+      <Text>{code}</Text>
+  </View>
+</TouchableHighlight>
 )
 
 const _keyExtractor = item => item.code
@@ -26,13 +35,39 @@ export default function ZipCodeScreen() {
     const navigation = useNavigation()
     return (
         <View>
+          <ImageBackground source={require('../bg-home.jpg')} style={styles.container}>
             <FlatList
                 data={availableZipItems}
                 keyExtractor={_keyExtractor}
                 renderItem={({ item }) => <ZipItem {...item} navigation={navigation} />}
             />
             <StatusBar style="auto" />
+            </ImageBackground>
         </View>
     );
-
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      padding: 25,
+    },
+
+    /*backdrop: {
+      alignItems: 'center',
+      backgroundColor: '#BDDAE6',
+      borderBottomWidth: 20,
+      width: '50%',
+      height: '70%',
+      fontSize: 50,
+      color: '#000',
+    },
+
+    backdropZip: {
+      flex: 1,
+      alignItems: 'center',
+      width: '100%',
+      height: '100%',
+    },*/
+});
