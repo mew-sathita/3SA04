9776a7cf0a74dc48285889b2862b377cf153d129
mew-sathita <s6135512032@phuket.psu.ac.th>
2020-10-02 12:49:25 +0700

    Exam 3SA04

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, TouchableHighlight } from 'react-native';
+//import CoC2 from './components/CoC2';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  showAlert = () => {
+    Alert.alert(   //alert
+
+
+      //'CoC2',  //text
+      //'Midterm Examination',  //text
+      //[
+        //{text: 'Ask me later', onPress: () => console.log('Ask me later pressed')},
+        //{text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+        //{text: 'OK', onPress: () => console.log('OK Pressed')},
+      //],
+      //{ cancelable: false }
+    )
+  };
+
+  render() {
+    return (
+        <View style={styles.container}>
+          <Text>Welcome to CoC</Text> 
+          <TouchableHighlight style={{margin: 30}}>
+            <Button
+            onPress={() => NavigationPreloadManager.NavigationPreloadManager('PAGE_NAME')}
+            title='SEC2'
+            color="#008080"/>
+          </TouchableHighlight>
+        </View>
+
+      //<View style={styles.container}>
+        //<Text>Welcome to CoC</Text>  
+        //<Button
+          //onPress={this.showAlert}
+          //title="SEC2"   //name botton
+          //color="green"     //color botton
+        ///>
+      //</View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#EBF4FA',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
