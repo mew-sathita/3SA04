48e064e39c06c0a3394365fb9d35fe18ac9be10b
mew-sathita <s6135512032@phuket.psu.ac.th>
2020-10-02 13:15:48 +0700

    hello world

--- App.js
+++ App.js
@@ -1,52 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Alert, TouchableHighlight } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
-  showAlert = () => {
-    Alert.alert(   //alert
-
-
-      //'CoC2',  //text
-      //'Midterm Examination',  //text
-      //[
-        //{text: 'Ask me later', onPress: () => console.log('Ask me later pressed')},
-        //{text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
-        //{text: 'OK', onPress: () => console.log('OK Pressed')},
-      //],
-      //{ cancelable: false }
-    )
-  };
-
-  render() {
-    return (
-        <View style={styles.container}>
-          <Text>Welcome to CoC</Text> 
-          <TouchableHighlight style={{margin: 30}}>
-            <Button
-            onPress={() => NavigationPreloadManager.NavigationPreloadManager('PAGE_NAME')}
-            title='SEC2'
-            color="#008080"/>
-          </TouchableHighlight>
-        </View>
-
-      //<View style={styles.container}>
-        //<Text>Welcome to CoC</Text>  
-        //<Button
-          //onPress={this.showAlert}
-          //title="SEC2"   //name botton
-          //color="green"     //color botton
-        ///>
-      //</View>
-    );
+export default function App() {
+  const doIt = () => {
+    console.log("Hello from console")
   }
+  return (
+    <View style={styles.container}>
+      <Text onPress={doIt}>Hello world</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#EBF4FA',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
-
